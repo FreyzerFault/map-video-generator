cebc75cac2e762ff962206446fd95555f759994c
David Martinez Alcantara <martinezalcantara29@gmail.com>
2025-06-23 10:53:50 +0200

    Preparando para empaquetar para su uso

---
+++ scripts/TEST - Generate Map Video.bat
@@ -0,0 +1,46 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB9VQAeDMn+7OoUO5/H+7P6IsXEufc4JV8Lr84ahHMEd40jldp8i2H9J2P8eDQFZfBunZws1rS5GsySmOMuQsh3kWXSk51twLWxwgmyQqy41ZcFpjswR7za3+EjwoKgRwUf4W6cxHGDuz+JhO8Zg
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB9VQAeDMn+7OoUO5/H+7P6IsXEufc4JV8Lr84ahHMEd40jldp8i2H9J2P8eDQFZfBunZws1rS5GsySmOMuQsh3kWXSk51twLWxwgmyQqy41ZcFpjswR7xSbz3m/0uU31Hb6TKgaECPMwLwoDsAE+AX6bVHN+g==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+::[Bat To Exe Converter]
+
+@echo on
+
+:: Script python map_video_generator.py
+:: Install Requirements.txt
+pip install -r requirements.txt
+
+set script_path=./src/main.py -ty
+
+python %script_path%
+
+pause
